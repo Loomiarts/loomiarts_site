c39ac5ae367122d038d041cc10ca59a4c502385d
Marcio Rosa <marciodrosa@gmail.com>
2024-06-16 13:20:40 -0300

    Add a widget test to check the navigation flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:loomiarts_site/main.dart';
+import 'package:presentation/presentation.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    //await tester.pumpWidget(const MyApp());
+  testWidgets('Navigation test', (WidgetTester tester) async {
+    await tester.pumpWidget(const SiteApp());
 
-    // Verify that our counter starts at 0.
-    //expect(find.text('0'), findsOneWidget);
-    //expect(find.text('1'), findsNothing);
+    await tester.tap(find.byIcon(Icons.language));
+    await tester.tap(find.text('English'));
+    expect(find.text('Game'), findsOneWidget);
+    expect(find.text('About'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    //await tester.tap(find.byIcon(Icons.add));
-    //await tester.pump();
+    await tester.tap(find.text('Game'));
+    expect(find.text('Squad 51 vs. the Flying Saucers'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    //expect(find.text('0'), findsNothing);
-    //expect(find.text('1'), findsOneWidget);
+    tester.pageBack();
+    await tester.tap(find.text('About'));
+    expect(find.text('About'), findsOneWidget);
   });
 }
